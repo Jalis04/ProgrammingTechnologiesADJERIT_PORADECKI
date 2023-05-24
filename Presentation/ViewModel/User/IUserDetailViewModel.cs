01313dfc9ff05b0886f95f56e75e7a240563574d
eryk-poradecki <eryk.poradecki@gmail.com>
2023-05-24 13:39:37 +0200

    feat: added presenntation viewmodel

---
+++ Presentation/ViewModel/User/IUserDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Presentation.Model.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Presentation.ViewModel.User
+{
+    public interface IUserDetailViewModel
+    {
+        static IUserDetailViewModel CreateViewModel(int id, string firstName, string lastName, IUserModelOperation model, IErrorInformer informer)
+        {
+            return new UserDetailViewModel(id, firstName, lastName, model, informer);
+        }
+
+        ICommand UpdateUser { get; set; }
+
+        int Id { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+    }
+}
---
+++ Presentation/ViewModel/User/IUserMasterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Presentation.Model.API;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Presentation.ViewModel.User
+{
+    public interface IUserMasterViewModel
+    {
+        static IUserMasterViewModel CreateViewModel(IUserModelOperation operation, IErrorInformer informer)
+        {
+            return new UserMasterViewModel(operation, informer);
+        }
+
+        ICommand CreateUser { get; set; }
+
+        ICommand RemoveUser { get; set; }
+
+        ObservableCollection<IUserDetailViewModel> Users { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        bool IsUserSelected { get; set; }
+
+        Visibility IsUserDetailVisible { get; set; }
+
+        IUserDetailViewModel SelectedDetailViewModel { get; set; }
+    }
+}
